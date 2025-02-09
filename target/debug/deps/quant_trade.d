78a7ba92d3b620f3a40662e45d854e6c1b8a70ef
codnomer <demirhanomer11@gmail.com>
2025-02-09 20:33:49 +0300

    Define limits, price ext.

---
+++ target/debug/deps/quant_trade.d
@@ -0,0 +1,5 @@
+C:\Users\demir\RustroverProjects\quant-trade\target\debug\deps\quant_trade.exe: src/main.rs
+
+C:\Users\demir\RustroverProjects\quant-trade\target\debug\deps\quant_trade.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/quant-trade.d
@@ -0,0 +1,1 @@
+C:\Users\demir\RustroverProjects\quant-trade\target\debug\quant-trade.exe: C:\Users\demir\RustroverProjects\quant-trade\src\main.rs
